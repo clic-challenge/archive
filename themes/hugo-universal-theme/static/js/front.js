1008756ad8f15522ce9c03fba72d51127b9a64ff
Jona Ballé <jona.balle@nyu.edu>
2025-02-13 17:51:04 -0500

    Updates Hugo Universal theme (and removes exampleSite).

--- themes/hugo-universal-theme/static/js/front.js
+++ themes/hugo-universal-theme/static/js/front.js
@@ -14,17 +14,19 @@ $(function () {
   fullScreenContainer()
   productDetailGallery(4000)
   menuSliding()
+  menuMouseOver()
   productDetailSizes()
   utils()
   animations()
   counters()
   demo()
-  contactForm()
+  contactFormAjax()
 })
 
 // Ajax contact
-function contactForm () {
-  var form = $('.contact-form')
+function contactFormAjax () {
+  const form = $('.contact-form-ajax')
+  if (typeof form === 'undefined') return false
   form.submit(function () {
     $this = $(this)
     $.post($(this).attr('action'),
@@ -49,11 +51,11 @@ function demo () {
 
   $('#colour').change(function () {
     if ($(this).val() !== '') {
-      var themeCSSpath = 'css/style.' + $(this).val() + '.css'
+      const themeCSSpath = 'css/style.' + $(this).val() + '.css'
 
       $('link#theme-stylesheet').attr('href', themeCSSpath)
 
-      $.cookie('themeCSSpath', themeCSSpath, {expires: 365, path: '/'})
+      $.cookie('themeCSSpath', themeCSSpath, { expires: 365, path: '/' })
     }
 
     return false
@@ -61,14 +63,14 @@ function demo () {
 
   $('#layout').change(function () {
     if ($(this).val() !== '') {
-      var themeLayout = $(this).val()
+      const themeLayout = $(this).val()
 
       $('body').removeClass('wide')
       $('body').removeClass('boxed')
 
       $('body').addClass(themeLayout)
 
-      $.cookie('themeLayout', themeLayout, {expires: 365, path: '/'})
+      $.cookie('themeLayout', themeLayout, { expires: 365, path: '/' })
     }
 
     return false
@@ -82,7 +84,7 @@ function sliderHomepage () {
 
     $('#slider').owlCarousel({
       autoPlay: 3000,
-      items: 8,
+      items: 4,
       itemsDesktopSmall: [900, 3],
       itemsTablet: [600, 3],
       itemsMobile: [500, 2]
@@ -94,37 +96,31 @@ function sliderHomepage () {
 function sliders () {
   if ($('.owl-carousel').length) {
     $('.customers').owlCarousel({
-      items: 8,
-      itemsDesktopSmall: [990, 8],
-      itemsTablet: [768, 3],
+      items: ($('.customers').attr('data-items') || 6),
+      slideSpeed: ($('.customers').attr('data-slide-speed') || 2000),
+      paginationSpeed: ($('.customers').attr('data-pagination-speed') || 1000),
+      autoPlay: $('.customers').attr('data-autoplay') === 'true',
+      itemsDesktopSmall: [990, 4],
+      itemsTablet: [768, 2],
       itemsMobile: [480, 1]
     })
 
     $('.testimonials').owlCarousel({
-      items: 8,
+      items: ($('.testimonials').attr('data-items') || 4),
+      slideSpeed: ($('.testimonials').attr('data-slide-speed') || 2000),
+      paginationSpeed: ($('.testimonials').attr('data-pagination-speed') || 1000),
+      autoPlay: $('.testimonials').attr('data-autoplay') === 'true',
       itemsDesktopSmall: [990, 3],
       itemsTablet: [768, 2],
       itemsMobile: [480, 1]
     })
 
-    $('.project').owlCarousel({
-      navigation: true, // Show next and prev buttons
-      navigationText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-      slideSpeed: 30,
-      paginationSpeed: 40,
-      autoPlay: true,
-      stopOnHover: true,
-      singleItem: true,
-      afterInit: '',
-      lazyLoad: true
-    })
-
     $('.homepage').owlCarousel({
       navigation: false, // Show next and prev buttons
-      navigationText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-      slideSpeed: 2000,
-      paginationSpeed: 1000,
-      autoPlay: true,
+      navigationText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],
+      slideSpeed: ($('.homepage').attr('data-slide-speed') || 2000),
+      paginationSpeed: ($('.homepage').attr('data-pagination-speed') || 1000),
+      autoPlay: ($('.homepage').attr('data-autoplay') || 'true') === 'true',
       stopOnHover: true,
       singleItem: true,
       lazyLoad: false,
@@ -158,10 +154,16 @@ function menuSliding () {
   })
 }
 
+function menuMouseOver () {
+  $('.mouseover .dropdown').hover(function () {
+    $('.dropdown-toggle', this).trigger('click')
+  })
+}
+
 /* animations */
 function animations () {
-  var delayTime = 0
-  $('[data-animate]').css({opacity: '0'})
+  let delayTime = 0
+  $('[data-animate]').css({ opacity: '0' })
   $('[data-animate]').waypoint(function () {
     delayTime += 150
     $(this).delay(delayTime).queue(function (next) {
@@ -178,7 +180,7 @@ function animations () {
   })
 
   $('[data-animate-hover]').hover(function () {
-    $(this).css({opacity: 1})
+    $(this).css({ opacity: 1 })
     $(this).addClass('animated')
     $(this).removeClass($(this).data('animate'))
     $(this).addClass($(this).data('animate-hover'))
@@ -189,12 +191,12 @@ function animations () {
 }
 
 function animationsSlider () {
-  var delayTimeSlider = 400
+  let delayTimeSlider = 400
 
   $('.owl-item:not(.active) [data-animate-always]').each(function () {
     $(this).removeClass('animated')
     $(this).removeClass($(this).data('animate-always'))
-    $(this).stop(true, true, true).css({opacity: 0})
+    $(this).stop(true, true, true).css({ opacity: 0 })
   })
 
   $('.owl-item.active [data-animate-always]').each(function () {
@@ -220,15 +222,17 @@ function counters () {
 /* picture zoom */
 function pictureZoom () {
   $('.product .image, .post .image, .photostream div').each(function () {
-    var imgHeight = $(this).find('img').height()
-    $(this).height(imgHeight)
+    const imgHeight = $(this).find('img').height()
+    if (imgHeight) {
+      $(this).height(imgHeight)
+    }
   })
 }
 
 /* full screen intro */
 function fullScreenContainer () {
-  var screenWidth = $(window).width() + 'px'
-  var screenHeight = '500px'
+  const screenWidth = $(window).width() + 'px'
+  let screenHeight = '500px'
 
   if ($(window).height() > 500) {
     screenHeight = $(window).height() + 'px'
@@ -246,7 +250,7 @@ function utils () {
 
   /* click on the box activates the radio */
   $('#checkout').on('click', '.box.shipping-method, .box.payment-method', function () {
-    var radio = $(this).find(':radio')
+    const radio = $(this).find(':radio')
     radio.prop('checked', true)
   })
 
@@ -263,8 +267,8 @@ function utils () {
 
   /* animated scrolling */
   $('.scroll-to, .scroll-to-top').click(function (event) {
-    var fullUrl = this.href
-    var parts = fullUrl.split('#')
+    const fullUrl = this.href
+    const parts = fullUrl.split('#')
 
     if (parts.length > 1) {
       scrollTo(fullUrl)
@@ -273,10 +277,10 @@ function utils () {
   })
 
   function scrollTo (fullUrl) {
-    var parts = fullUrl.split('#')
-    var trgt = parts[1]
-    var targetOffset = $('#' + trgt).offset()
-    var targetTop = targetOffset.top - 100
+    const parts = fullUrl.split('#')
+    const trgt = parts[1]
+    const targetOffset = $('#' + trgt).offset()
+    let targetTop = targetOffset.top - 100
 
     if (targetTop < 0) {
       targetTop = 0
@@ -291,7 +295,7 @@ function utils () {
 /* product detail gallery */
 function productDetailGallery (confDetailSwitch) {
   $('.thumb:first').addClass('active')
-  var timer = setInterval(autoSwitch, confDetailSwitch)
+  let timer = setInterval(autoSwitch, confDetailSwitch)
 
   $('.thumb').click(function (e) {
     switchImage($(this))
@@ -307,7 +311,7 @@ function productDetailGallery (confDetailSwitch) {
   })
 
   function autoSwitch () {
-    var nextThumb = $('.thumb.active').closest('div').next('div').find('.thumb')
+    let nextThumb = $('.thumb.active').closest('div').next('div').find('.thumb')
     if (nextThumb.length === 0) {
       nextThumb = $('.thumb:first')
     }
@@ -316,7 +320,7 @@ function productDetailGallery (confDetailSwitch) {
 
   function switchImage (thumb) {
     $('.thumb').removeClass('active')
-    var bigUrl = thumb.attr('href')
+    const bigUrl = thumb.attr('href')
     thumb.addClass('active')
     $('#mainImage img').attr('src', bigUrl)
   }
@@ -335,8 +339,8 @@ function productDetailSizes () {
 
 $.fn.alignElementsSameHeight = function () {
   $('.same-height-row').each(function () {
-    var maxHeight = 0
-    var children = $(this).find('.same-height')
+    let maxHeight = 0
+    let children = $(this).find('.same-height')
     children.height('auto')
 
     if ($(window).width() > 768) {
@@ -360,7 +364,7 @@ $.fn.alignElementsSameHeight = function () {
   })
 }
 
-var windowWidth
+let windowWidth
 $(function () {
   windowWidth = $(window).width()
 
@@ -369,7 +373,7 @@ $(function () {
 })
 
 $(window).resize(function () {
-  var newWindowWidth = $(window).width()
+  const newWindowWidth = $(window).width()
 
   if (windowWidth !== newWindowWidth) {
     setTimeout(function () {
